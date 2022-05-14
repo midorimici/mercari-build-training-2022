56dd97eec7aacf1b3fdcefdb2782f464520d7960
mici <60635066+midorimici@users.noreply.github.com>
2022-05-14 21:26:51 +0900

    Add a new category to DB

--- go/app/db.go
+++ go/app/db.go
@@ -55,8 +55,38 @@ func addItemToDB(name, category, imageFilename string) (int, error) {
 	}
 	defer db.Close()
 
+	// Check if the category exists
+	var categoryCount int
+	countRow := db.QueryRow("SELECT COUNT(*) FROM category WHERE name = ?", category)
+	if err := countRow.Scan(&categoryCount); err != nil {
+		return 0, fmt.Errorf("addItemToDB failed: %w", err)
+	}
+
+	var categoryId int
+	if categoryCount > 0 {
+		// When the category already exists
+		categoryRow := db.QueryRow("SELECT id FROM category WHERE name = ?", category)
+		if err := categoryRow.Scan(&categoryId); err != nil {
+			return 0, fmt.Errorf("addItemToDB failed: %w", err)
+		}
+	} else {
+		// When the category does not exist
+		// Create a new category in DB
+		r, err := db.Exec("INSERT INTO category (name) values (?)", category)
+		if err != nil {
+			return 0, fmt.Errorf("addItemToDB failed: %w", err)
+		}
+
+		id, err := r.LastInsertId()
+		if err != nil {
+			return 0, fmt.Errorf("addItemToDB failed: %w", err)
+		}
+
+		categoryId = int(id)
+	}
+
 	// Insert into DB
-	r, err := db.Exec("INSERT INTO items (name, category, image_filename) values (?, ?, ?)", name, category, imageFilename)
+	r, err := db.Exec("INSERT INTO items (name, category_id, image_filename) values (?, ?, ?)", name, categoryId, imageFilename)
 	if err != nil {
 		return 0, fmt.Errorf("addItemToDB failed: %w", err)
 	}
