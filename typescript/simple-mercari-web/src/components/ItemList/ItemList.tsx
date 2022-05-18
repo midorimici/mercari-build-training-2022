d342e2b2e9e502aaccd9544028e40d26b7f15703
mici <60635066+midorimici@users.noreply.github.com>
2022-05-18 22:31:44 +0900

    Format by Prettier

--- typescript/simple-mercari-web/src/components/ItemList/ItemList.tsx
+++ typescript/simple-mercari-web/src/components/ItemList/ItemList.tsx
@@ -5,7 +5,7 @@ interface Item {
   name: string;
   category: string;
   image: string;
-};
+}
 
 const server = process.env.API_URL || 'http://127.0.0.1:9000';
 const placeholderImage = process.env.PUBLIC_URL + '/logo192.png';
@@ -17,49 +17,48 @@ interface Prop {
 
 export const ItemList: React.FC<Prop> = (props) => {
   const { reload = true, onLoadCompleted } = props;
-  const [items, setItems] = useState<Item[]>([])
+  const [items, setItems] = useState<Item[]>([]);
   const fetchItems = () => {
-    fetch(server.concat('/items'),
-    {
+    fetch(server.concat('/items'), {
       method: 'GET',
       mode: 'cors',
-      headers : {
+      headers: {
         'Content-Type': 'application/json',
-        'Accept': 'application/json'
+        Accept: 'application/json',
       },
     })
-      .then(response => response.json())
-      .then(data => {
-        console.log('GET success:',data);
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('GET success:', data);
         setItems(data.items);
         onLoadCompleted && onLoadCompleted();
       })
-      .catch(error => {
-        console.error('GET error:',error)
-      })
-  }
+      .catch((error) => {
+        console.error('GET error:', error);
+      });
+  };
 
   useEffect(() => {
     if (reload) {
       fetchItems();
     }
   }, [reload]);
-  
+
   return (
     <div>
-      { items.map((item) => {
+      {items.map((item) => {
         return (
-          <div key={item.id} className='ItemList'>
+          <div key={item.id} className="ItemList">
             {/* TODO: Task 1: Replace the placeholder image with the item image */}
-            <img src={placeholderImage}/>
+            <img src={placeholderImage} />
             <p>
-            <span>Name: {item.name}</span>
-            <br/>
-            <span>Category: {item.category}</span>
+              <span>Name: {item.name}</span>
+              <br />
+              <span>Category: {item.category}</span>
             </p>
           </div>
-        )
+        );
       })}
     </div>
-  )
+  );
 };
--- typescript/simple-mercari-web/src/components/Listing/Listing.tsx
+++ typescript/simple-mercari-web/src/components/Listing/Listing.tsx
@@ -9,46 +9,59 @@ interface Prop {
 export const Listing: React.FC<Prop> = (props) => {
   const { onListingCompleted } = props;
   const initialState = {
-    name: "",
-    category: "",
-    image: "",
+    name: '',
+    category: '',
+    image: '',
   };
   const [values, setValues] = useState(initialState);
-  
+
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      setValues({ ...values, [event.target.name]: event.target.value });
+    setValues({ ...values, [event.target.name]: event.target.value });
   };
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    const data = new FormData()
-    data.append('name', values.name)
-    data.append('category', values.category)
-    data.append('image', values.image)
+    event.preventDefault();
+    const data = new FormData();
+    data.append('name', values.name);
+    data.append('category', values.category);
+    data.append('image', values.image);
 
     fetch(server.concat('/items'), {
       method: 'POST',
       mode: 'cors',
       body: data,
     })
-    .then(response => response.json())
-    .then(data => {
-      console.log('POST success:', data);
-      onListingCompleted && onListingCompleted();
-    })
-    .catch((error) => {
-      console.error('POST error:', error);
-    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('POST success:', data);
+        onListingCompleted && onListingCompleted();
+      })
+      .catch((error) => {
+        console.error('POST error:', error);
+      });
   };
   return (
-    <div className='Listing'>
+    <div className="Listing">
       <form onSubmit={onSubmit}>
         <div>
-            <input type='text' name='name' id='name' placeholder='name' onChange={onChange} required/>
-            <input type='text' name='category' id='category' placeholder='category' onChange={onChange}/>
-            <input type='file' name='image' id='image' placeholder='image' onChange={onChange}/>
-            <button type='submit'>List this item</button>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="name"
+            onChange={onChange}
+            required
+          />
+          <input
+            type="text"
+            name="category"
+            id="category"
+            placeholder="category"
+            onChange={onChange}
+          />
+          <input type="file" name="image" id="image" placeholder="image" onChange={onChange} />
+          <button type="submit">List this item</button>
         </div>
       </form>
     </div>
   );
-}
+};
